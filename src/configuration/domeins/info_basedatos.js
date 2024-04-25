5d617376682be507be8108f35b8a6c1455130f3a
Andres <andres.cardenasa@usantoto.edu.co>
2024-04-25 15:27:59 -0500

    Upload backend nodejs for guia03

---
+++ src/configuration/domeins/info_basedatos.js
@@ -0,0 +1,8 @@
+"use strict";
+// export default {
+//     user: 'user_angular',
+//     host: 'localhost',
+//     database: 'bd_angular_guias003',
+//     password: '123456',
+//     port: 5432
+// }
