27d2fb8e3080e3e6d9926d7f62fc3eadf7d1e6b5
Andres <andres.cardenasa@usantoto.edu.co>
2024-04-25 15:51:13 -0500

    fix: cors?

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const Server_1 = __importDefault(require("./configuration/api/Server"));
+const Server_1 = __importDefault(require("./src/configuration/api/Server"));
 const server = new Server_1.default();
 server.start();
